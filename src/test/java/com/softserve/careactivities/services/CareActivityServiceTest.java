1ac18fbbdf7d5787651c5397e281bf854f9d1775
Yehor Horbatenko <Yehor.Horbatenko@davita.com>
2021-08-30 14:24:55 +0300

    Add shouldGetCareActivityById test method

--- src/test/java/com/softserve/careactivities/services/CareActivityServiceTest.java
+++ src/test/java/com/softserve/careactivities/services/CareActivityServiceTest.java
@@ -6,6 +6,8 @@ import com.softserve.careactivities.domain.mappers.CareActivityMapper;
 import com.softserve.careactivities.feign_clients.PatientsClient;
 import com.softserve.careactivities.repositories.CareActivityRepository;
 import com.softserve.careactivities.services.implementations.CareActivityServiceImpl;
+import com.softserve.careactivities.utils.exceptions.CustomEntityNotFoundException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,9 +15,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -34,12 +38,57 @@ class CareActivityServiceTest {
     @InjectMocks
     CareActivityServiceImpl careActivityService;
 
+    static CareActivity careActivity = new CareActivity();
+
+    static CareActivityDTO careActivityDTO = new CareActivityDTO();
+
+    static LocalDateTime localDateTime;
+
+    static ZonedDateTime zonedDateTime;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         careActivityService = new CareActivityServiceImpl(careActivityRepository, patientsClient, careActivityMapper);
+
+        localDateTime = LocalDateTime.of(2000, 10, 10, 10, 10);
+        zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+
+        careActivity.setCareActivityId("12345");
+        careActivity.setCareActivityComment("comment");
+        careActivity.setMasterPatientIdentifier("MPI");
+        careActivity.setCreateDateTimeGMT(localDateTime);
+        careActivity.setUpdateDateTimeGMT(localDateTime);
+        careActivity.setState(CareActivity.StateEnum.ACTIVE);
+
+        careActivityDTO.setCareActivityId("12345");
+        careActivityDTO.setCareActivityComment("comment");
+        careActivityDTO.setMasterPatientIdentifier("MPI");
+        careActivityDTO.setCreateDateTimeGMT(zonedDateTime);
+        careActivityDTO.setUpdateDateTimeGMT(zonedDateTime);
+        careActivityDTO.setState(CareActivity.StateEnum.ACTIVE);
     }
 
+    @AfterEach
+    void tearDown() {
+
+        localDateTime = null;
+        zonedDateTime = null;
+
+        careActivity.setCareActivityId(null);
+        careActivity.setCareActivityComment(null);
+        careActivity.setMasterPatientIdentifier(null);
+        careActivity.setCreateDateTimeGMT(null);
+        careActivity.setUpdateDateTimeGMT(null);
+        careActivity.setState(null);
+
+        careActivityDTO.setCareActivityId(null);
+        careActivityDTO.setCareActivityComment(null);
+        careActivityDTO.setMasterPatientIdentifier(null);
+        careActivityDTO.setCreateDateTimeGMT(null);
+        careActivityDTO.setUpdateDateTimeGMT(null);
+        careActivityDTO.setState(null);
+    }
 
     @Test
     void shouldGetAllCareActivities() {
@@ -61,6 +110,23 @@ class CareActivityServiceTest {
 
         assertEquals(3, careActivityDTOList.size());
         verify(careActivityRepository, times(1)).findAll();
+    }
+
+    @Test
+    void shouldGetCareActivityById() throws CustomEntityNotFoundException {
+        CareActivity expected = careActivity;
+
+        CareActivityDTO expectedDTO = careActivityDTO;
+
+        when(careActivityRepository.findById(expected.getCareActivityId())).thenReturn(Optional.of(expected));
+
+        when(careActivityMapper.CAToCADTO(expected)).thenReturn(expectedDTO);
+
+        CareActivityDTO actual = careActivityService.getCareActivityById(expected.getCareActivityId());
+
+        assertEquals(actual, careActivityMapper.CAToCADTO(expected));
+        verify(careActivityRepository, times(1)).findById(expected.getCareActivityId());
+
     }
 
 }
