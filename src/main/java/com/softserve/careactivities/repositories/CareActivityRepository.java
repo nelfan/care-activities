46a6fe92ca5e77a65c8af58d3d3f1d83175e02ae
Yehor Horbatenko <Yehor.Horbatenko@davita.com>
2021-09-22 18:28:33 +0300

    delete all

--- src/main/java/com/softserve/careactivities/repositories/CareActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.careactivities.repositories;
-
-import com.google.cloud.spring.data.spanner.repository.SpannerRepository;
-import com.softserve.careactivities.domain.entities.CareActivity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CareActivityRepository extends SpannerRepository<CareActivity, String> {
-}
